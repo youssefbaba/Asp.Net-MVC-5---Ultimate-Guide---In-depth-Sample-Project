d452f7f5a2d30959adc6c880e1e666132a41d930
youssefbaba <ybaba398@gmail.com>
2023-02-10 14:16:56 +0100

    RedirectToRouteResult

---
+++ MyFirstApplication/MyFirstApplication/Controllers/AccountController.cs
@@ -0,0 +1,32 @@
+﻿using System.Web.Mvc;
+
+namespace MyFirstApplication.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        // GET: /account/login
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        // POST:
+        public ActionResult Login(string userName, string passWord)
+        {
+            if (userName == "admin" && passWord == "manager")
+            {
+                return RedirectToAction("Dashboard", "Admin");
+            };
+            return RedirectToAction("InvalidLogin");
+        }
+
+        [HttpGet]
+        // GET: /account/invalidlogin
+        public ActionResult InvalidLogin()
+        {
+            return View();
+        }
+    }
+}
---
+++ MyFirstApplication/MyFirstApplication/Controllers/AdminController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace MyFirstApplication.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: /admin/dashboard
+        public ActionResult Dashboard()
+        {
+            return View();
+        }
+    }
+}
