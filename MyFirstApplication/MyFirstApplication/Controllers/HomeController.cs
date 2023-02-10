253a380a1f698a2cf75207dfd93505aa0758864e
youssefbaba <ybaba398@gmail.com>
2023-02-10 02:24:55 +0100

    Getting Started

---
+++ MyFirstApplication/MyFirstApplication/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System.Web.Mvc;
+
+namespace MyFirstApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        // GET: /home/index
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        // GET: /home/products
+        public ActionResult products()
+        {
+            return View("OurCompanyProducts");
+        }
+
+        [HttpGet]
+        // GET: /home/contact
+        public ActionResult contact()
+        {
+            return View();
+        }
+    }
+}
