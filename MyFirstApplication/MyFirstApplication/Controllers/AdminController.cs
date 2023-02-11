c0c7ba696228aad3a241d9df6de8a97d56b773b9
youssefbaba <ybaba398@gmail.com>
2023-02-11 10:15:49 +0100

    Shared Views

--- MyFirstApplication/MyFirstApplication/Controllers/AdminController.cs
+++ MyFirstApplication/MyFirstApplication/Controllers/AdminController.cs
@@ -4,8 +4,16 @@ namespace MyFirstApplication.Controllers
 {
     public class AdminController : Controller
     {
+        [HttpGet]
         // GET: /admin/dashboard
         public ActionResult Dashboard()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        // GET: /admin/contact
+        public ActionResult Contact()
         {
             return View();
         }
