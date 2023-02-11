e61f49c1f87555d95a6c79281d1088677449c633
youssefbaba <ybaba398@gmail.com>
2023-02-11 12:39:59 +0100

    Layout Views

---
+++ LayoutViewsExample/LayoutViewsExample/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System.Web.Mvc;
+
+namespace LayoutViewsExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        // GET: /home/index
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        // GET: /home/about
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        // GET: /home/contact
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        // GET: /home/profile
+        public new ActionResult Profile()
+        {
+            return View();
+        }
+    }
+}
