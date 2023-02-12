ebce13fd74f97ad31c93c4acf8799c705614e58f
youssefbaba <ybaba398@gmail.com>
2023-02-12 01:04:27 +0100

    URL Routing - Default Routing - ID Parameter - ID as string Parameter

--- LayoutViewsExample/LayoutViewsExample/Controllers/ProductsController.cs
+++ LayoutViewsExample/LayoutViewsExample/Controllers/ProductsController.cs
@@ -1,4 +1,7 @@
-﻿using System.Web.Mvc;
+﻿using LayoutViewsExample.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
 
 namespace LayoutViewsExample.Controllers
 {
@@ -8,15 +11,57 @@ namespace LayoutViewsExample.Controllers
         // GET: /products/index
         public ActionResult Index()
         {
-            var products = new[]
+            var products = GetProducts();
+            return View(products);
+        }
+
+        [HttpGet]
+        // GET : /products/details/1
+        public ActionResult Details(int? id)
+        {
+            if (!id.HasValue)
             {
-                new { ProductId = 100 , ProductName = "Camera" , Price = 1500},
-                new { ProductId = 101 , ProductName = "Galaxy S10" , Price = 1900},
-                new { ProductId = 102 , ProductName = "Hp Printer" , Price = 1000},
-                new { ProductId = 103 , ProductName = "Hp Laptop" , Price = 2500}
+                return Content("Please pass any ProductId");
+            }
+            var products = GetProducts();
+            var product = products.SingleOrDefault(p => p.ProductId == id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        [HttpGet]
+        // GET : /products/getProductid/Camera
+        public ActionResult GetProductId(string id)
+        {
+            if (string.IsNullOrWhiteSpace(id))
+            {
+                return Content("Please pass any ProductName");
+            }
+            var products = GetProducts();
+            var product = products.SingleOrDefault(p => p.ProductName == id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+
+            return Content($"Product Name = {product.ProductName}");
+        }
+
+        [NonAction]
+        private IEnumerable<Product> GetProducts()
+        {
+
+            var products = new List<Product>()
+            {
+                new Product() { ProductId = 1, ProductName = "Camera", Price = 1500 },
+                new Product() { ProductId = 2, ProductName = "Galaxy S10", Price = 1900 },
+                new Product() { ProductId = 3, ProductName = "Hp Printer", Price = 1000 },
+                new Product() { ProductId = 4, ProductName = "Hp Laptop", Price = 2500 }
             };
-            ViewBag.Products = products;
-            return View();
+            return products;
         }
     }
 }
---
+++ LayoutViewsExample/LayoutViewsExample/Models/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace LayoutViewsExample.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public int Price { get; set; }
+    }
+}
