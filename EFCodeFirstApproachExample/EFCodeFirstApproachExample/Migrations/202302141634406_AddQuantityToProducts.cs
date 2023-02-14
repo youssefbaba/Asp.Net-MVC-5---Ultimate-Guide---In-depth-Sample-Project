8499f7c33bf89924bd29d10168604442d9754d00
youssefbaba <ybaba398@gmail.com>
2023-02-14 17:37:54 +0100

    EF Code-First Migrations Manually

---
+++ EFCodeFirstApproachExample/EFCodeFirstApproachExample/Migrations/202302141634406_AddQuantityToProducts.cs
@@ -0,0 +1,18 @@
+﻿namespace EFCodeFirstApproachExample.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddQuantityToProducts : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Products", "Quantity", c => c.Decimal(precision: 18, scale: 2));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Products", "Quantity");
+        }
+    }
+}
