4f8420e59eae17537f23daaffeb7373e69865481
youssefbaba <ybaba398@gmail.com>
2023-02-15 15:19:43 +0100

    Custom HTML Helpers

---
+++ EFCodeFirstApproachExample/EFCodeFirstApproachExample/HtmlHelpers/CustomHtmlHelper.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+
+namespace EFCodeFirstApproachExample.HtmlHelpers
+{
+    public static class CustomHtmlHelper
+    {
+        public static MvcHtmlString File(this HtmlHelper htmlHelper, string classTag, string idTag, string nameTag)
+        {
+            TagBuilder tag = new TagBuilder("input");
+            tag.MergeAttribute("type", "file");
+            tag.MergeAttribute("class", classTag);
+            tag.MergeAttribute("id", idTag);
+            tag.MergeAttribute("name", nameTag);
+            return new MvcHtmlString(tag.ToString(TagRenderMode.SelfClosing));
+        }
+    }
+}
